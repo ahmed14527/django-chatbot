a74f18d1406b780b3fe38e16d17942ed16d7ce5e
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-11-08 04:57:44 +0200

    FFF

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chatbot',
+    'chatbot_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- django_chatbot/urls.py
+++ django_chatbot/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chatbot.urls'))
+    path('', include('chatbot.urls')),
+    path('api/', include('chatbot_app.urls')),
+
 ]
